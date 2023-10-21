52e09b8dd3f2e9926a9f0ce9eb420a51a515c33d
ATkabladze <alinatkabladze@gmail.com>
2023-10-21 12:43:57 +0400

    Add tests for Meeting1

---
+++ src/test/java/Meeting1/BooksApi.java
@@ -0,0 +1,41 @@
+package Meeting1;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BooksApi {
+    @Test
+    public void getBooks() {
+        RestAssured.
+                given().
+                when().
+                get("https://bookstore.toolsqa.com/BookStore/v1/Books").
+                then().
+                assertThat()
+                .body("books[0].author",equalTo("Richard E. Silverman"));
+    }
+    @Test
+    public void getBook() {
+        RestAssured.
+                given().
+                when().
+                get("https://bookstore.toolsqa.com/BookStore/v1/Book?ISBN=9781449325862").
+                then().
+                assertThat()
+                .body("book.author",equalTo("Richard E. Silverman"));
+    }
+    @Test
+    public void getBookSecondWay() {
+        RestAssured.
+                given().
+                param("ISBN","9781449325862").
+                when().
+                get("https://bookstore.toolsqa.com/BookStore/v1/Book").
+                then().
+                assertThat()
+                .body("book.author",equalTo("Richard E. Silverman"));
+    }
+}
