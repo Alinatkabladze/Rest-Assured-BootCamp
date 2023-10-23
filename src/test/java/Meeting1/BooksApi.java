d45de41db4cd3d287e4cddb920f4abcb5c12e398
ATkabladze <alinatkabladze@gmail.com>
2023-10-23 23:28:21 +0400

    Fix test defects

--- src/test/java/Meeting1/BooksApi.java
+++ src/test/java/Meeting1/BooksApi.java
@@ -1,12 +1,17 @@
 package Meeting1;
 
 import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
 public class BooksApi {
+    @BeforeMethod
+    public void setup(){
+        RestAssured.useRelaxedHTTPSValidation();
+    }
     @Test
     public void getBooks() {
         RestAssured.
@@ -25,7 +30,7 @@ public class BooksApi {
                 get("https://bookstore.toolsqa.com/BookStore/v1/Book?ISBN=9781449325862").
                 then().
                 assertThat()
-                .body("book.author",equalTo("Richard E. Silverman"));
+                .body("author",equalTo("Richard E. Silverman"));
     }
     @Test
     public void getBookSecondWay() {
@@ -36,6 +41,6 @@ public class BooksApi {
                 get("https://bookstore.toolsqa.com/BookStore/v1/Book").
                 then().
                 assertThat()
-                .body("book.author",equalTo("Richard E. Silverman"));
+                .body("author",equalTo("Richard E. Silverman"));
     }
 }
